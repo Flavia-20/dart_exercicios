7dbca89bf8c63b0390f9d76879cda2b5f08676e5
joaodionizio <joao.dionizio@souunit.com.br>
2026-04-01 18:06:05 -0300

    adicionando validaçoes

--- exercicio_12.dart
+++ exercicio_12.dart
@@ -1,10 +1,31 @@
 import 'dart:io';
 
 void main() {
-  print('Digite um número inteiro:');
-  int numero = int.parse(stdin.readLineSync()!);
+  String entrada = '';
+
+  while (true) {
+    print('Digite um número inteiro:');
+    entrada = (stdin.readLineSync() ?? '').trim();
+
+    if (entrada.isEmpty) {
+      print('Você precisa digitar um número.\n');
+    } else if (int.tryParse(entrada) == null) {
+      print('Entrada inválida. Digite apenas números inteiros.\n');
+    } else if (int.parse(entrada) < 0) {
+      print('Digite um número inteiro positivo.\n');
+    } else {
+      break;
+    }
+  }
+
+  int numero = int.parse(entrada);
 
   print('Número invertido:');
+
+  if (numero == 0) {
+    print('0');
+    return;
+  }
 
   while (numero > 0) {
     int digito = numero % 10;
