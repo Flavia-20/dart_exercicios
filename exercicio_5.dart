14f1b79916d74ee1db3a93a551d2b3ee14417abb
Flavia-20 <flaviamonique38@gmail.com>
2026-04-02 23:46:19 -0300

    correção nas validações

--- exercicio_5.dart
+++ exercicio_5.dart
@@ -1,61 +1,84 @@
-void main() {
-  
-  List<Map<String, dynamic>> bebes = [
-    {
-      'nome': 'Andre',
-      'sexo': 'M',
-      'peso': 2.0
-    },
-    {
-      'nome': 'Maria',
-      'sexo': 'F',
-      'peso': 3.0
-    },
-    {
-      'nome': 'Joao',
-      'sexo': 'M',
-      'peso': 1.0
-    },
-    {
-      'nome': 'MAteus',
-      'sexo': 'M',
-      'peso': 4.0
-    },
-    {
-      'nome': 'Ana',
-      'sexo': 'F',
-      'peso': 5.0
-    },
-    
-  ];
+import 'dart:io';
+
+main(){
 
   double maior_peso_f = 0.0;
   String? nome_f;
 
-  int total_bebes = bebes.length;
+  int total_bebes = 0;
   double bebes_baixo_peso = 0;
   double bebes_peso_medio = 0;
   double bebes_peso_alto = 0;
 
+  String? funcaoNomeBebe(String? nome) {
+      if(nome == null || nome == "" || RegExp(r'[0-9]').hasMatch(nome) ) {
+      print("Nome inválido. Por favor, digite um nome válido.");
+      String? nome = stdin.readLineSync();
+      if (nome == 'FIM') {
+        print('Programa encerrado.');
+        exit(0);
+      }
+      return funcaoNomeBebe(nome);
+    }
+    return nome;
+  }
 
-  for (var bebe in bebes) {
-    double peso = bebe['peso'];
-    if (peso < 2.0) {
+  String? funcaoSexoBebe(String? sexo) {
+    if(sexo == null || (sexo.toUpperCase() != 'M' && sexo.toUpperCase() != 'F' || sexo == "")) {
+      print("Sexo inválido. Por favor, digite 'M' para masculino ou 'F' para feminino.");
+      String? sexo = stdin.readLineSync();
+      return funcaoSexoBebe(sexo);
+    }
+    return sexo.toUpperCase();
+  }
+
+String? funcaoPesoBebe(String? peso) {
+    if (peso == null || double.tryParse(peso) == null || double.parse(peso) <= 0 || double.parse(peso) >= 10 || peso == "" || RegExp(r'[a-zA-Z]').hasMatch(peso)) {
+      print("Peso inválido. Por favor, digite um número positivo.");
+      String? peso = stdin.readLineSync();
+      return funcaoPesoBebe(peso);
+    }
+    return peso;
+  }
+
+
+while (true) {
+  print("Digite o nome do bebe: ");
+  String? nome = stdin.readLineSync();
+  if (nome == 'FIM') {
+    print('Programa encerrado.');
+    break;
+  }
+  String? retornoNomeBebe = funcaoNomeBebe(nome);
+
+
+  print("Digite o sexo do candidato (M/F): ");
+  String? sexo = stdin.readLineSync();
+  String? retornoSexoBebe = funcaoSexoBebe(sexo);
+
+
+  print("Digite o peso do bebê: ");
+  String? peso = stdin.readLineSync();
+  String? retornoPesoBebe = funcaoPesoBebe(peso);
+ 
+  if(double.parse(retornoPesoBebe!) < 2.0) {
       bebes_baixo_peso++;
-      print("Bebê ${bebe['nome']}, sexo ${bebe['sexo']}, tem baixo peso");
-    } else if (peso >= 2.0 && peso <= 4.0) {
+      print("==== Bebê $retornoNomeBebe, sexo $retornoSexoBebe, tem baixo peso");
+    } else if (double.parse(retornoPesoBebe) >= 2.0 && double.parse(retornoPesoBebe) <= 4.0) {
       bebes_peso_medio++;
-      print("Bebê ${bebe['nome']}, sexo ${bebe['sexo']}, tem peso médio");
+      print("==== Bebê $retornoNomeBebe, sexo $retornoSexoBebe, tem peso médio");
     } else {
       bebes_peso_alto++;
-      print("Bebê ${bebe['nome']}, sexo ${bebe['sexo']}, tem alto peso");
+      print("==== Bebê $retornoNomeBebe, sexo $retornoSexoBebe, tem alto peso");
     }
 
   
-    if (bebe['sexo'] == 'F' && peso > maior_peso_f) {
-      maior_peso_f = peso;
-      nome_f = bebe['nome'];
+    if (retornoSexoBebe == 'F' && double.parse(retornoPesoBebe) > maior_peso_f) {
+      maior_peso_f = double.parse(retornoPesoBebe);
+      nome_f = retornoNomeBebe;
     }
+
+    total_bebes++;
   }
 
   double percentual_baixo_peso = (bebes_baixo_peso / total_bebes) * 100;
@@ -66,4 +89,5 @@ void main() {
   print('Percentual de bebês com peso médio: $percentual_peso_medio%');
   print('Percentual de bebês com alto peso: $percentual_peso_alto%');
   print('Bebê do sexo feminino com maior peso: $nome_f');
+
 }
