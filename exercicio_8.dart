df5c94d049ae8fb42c0929e643fb613cf1968145
Flavia-20 <flaviamonique38@gmail.com>
2026-04-03 20:19:06 -0300

    correção validação

--- exercicio_8.dart
+++ exercicio_8.dart
@@ -1,92 +1,161 @@
-void main() {
-
-  List<Map<String, dynamic>> alunos = [
-    {
-      'codigo': '1234',
-      'curso': 'CC-Ciência da Computação',
-      'nome': 'Andre',
-      'sexo': 'M',
-      'pontuação': 2500
-    },
-    {
-      'codigo': '5678',
-      'curso': 'SI-Sistemas de Informação',
-      'nome': 'Maria',
-      'sexo': 'F',
-      'pontuação': 2800
-    },
-    {
-      'codigo': '9012',
-      'curso': 'CC-Ciência da Computação',
-      'nome': 'Joao',
-      'sexo': 'M',
-      'pontuação': 2200
-    },
-    {
-      'codigo': '3456',
-      'curso': 'CC-Ciência da Computação',
-      'nome': 'MAteus',
-      'sexo': 'M',
-      'pontuação': 3500
-    },
-    {
-      'codigo': '7890',
-      'curso': 'SI-Sistemas de Informação',
-      'nome': 'Ana',
-      'sexo': 'F',
-      'pontuação': 2800
-    },
-    {
-      'codigo': '1122',
-      'curso': 'CC-Ciência da Computação',
-      'nome': 'Carlos', 
-      'sexo': 'M',
-      'pontuação': 2800
-    },
-    {
-      'codigo': '3344',
-      'curso': 'SI-Sistemas de Informação',
-      'nome': 'Pedro', 
-      'sexo': 'M',
-      'pontuação': 2400
-    },
-  ];
+import 'dart:io';
+main() {
 
   int maior_pontuacao_m = 0;
   int menor_pontuacao_m = 5000;
   String aluno_menor_pontuacao_m = "";
 
-  int alunas_m = 0;
+  int alunas_f = 0;
   int alunos_m = 0;
 
-  var alunos_com_mais_de_2500_pontos = [];
+  List<Map<String, dynamic>> alunos2500 = [];
 
-  for(var aluno in alunos){
-    if(aluno['pontuação'] > 2500 && aluno['curso'] == 'CC-Ciência da Computação'){
-      alunos_com_mais_de_2500_pontos.add(aluno);
+  Set<String> codigos = {};
+
+String? funcaoCodigo(String? codigo, Set<String> codigos) {
+
+
+  if (codigos.contains(codigo)) {
+      print("Código já existe! Digite outro.");
+      String? novoCodigo = stdin.readLineSync();
+      return funcaoCodigo(novoCodigo, codigos);
+  }
+
+  if (codigo == null || codigo == "" || RegExp(r'[a-zA-Z]').hasMatch(codigo) || codigo.length != 4) {
+    print("Código inválido. Por favor, digite um código válido ou '0000' para sair.");
+    String? novoCodigo = stdin.readLineSync();
+
+    if (novoCodigo == "0000") {
+      print('Programa encerrado.');
+      exit(0);
+    }
+
+    return funcaoCodigo(novoCodigo, codigos);
+  }
+
+  codigos.add(codigo);
+  return codigo;
+}
+
+String? funcaoNome(String? nome) {
+      if(nome == null || nome == "" || RegExp(r'[0-9]').hasMatch(nome) ) {
+      print("Nome inválido. Por favor, digite um nome válido.");
+      String? nome = stdin.readLineSync();
+      return funcaoNome(nome);
+    }
+    return nome;
+}
+
+String? funcaoSexo(String? sexo) {
+    if(sexo == null || (sexo.toUpperCase() != 'M' && sexo.toUpperCase() != 'F' || sexo == "")) {
+      print("Sexo inválido. Por favor, digite 'M' para masculino ou 'F' para feminino.");
+      String? sexo = stdin.readLineSync();
+      return funcaoSexo(sexo);
+    }
+    return sexo.toUpperCase();
+}
+
+String? funcaoPontuacao(String? pontuacao) {
+    if (pontuacao == null || int.tryParse(pontuacao) == null || int.parse(pontuacao) < 0 || int.parse(pontuacao) > 5000 || pontuacao == "") {
+      print("Pontuação inválida. Por favor, digite um número entre 0 e 5000.");
+      String? pontuacao = stdin.readLineSync();
+      return funcaoPontuacao(pontuacao);
+    }
+    return pontuacao;
+}
+
+String removerAcentos(String texto) {
+  var comAcento = 'áàãâäéèêëíìîïóòõôöúùûüçÁÀÃÂÄÉÈÊËÍÌÎÏÓÒÕÔÖÚÙÛÜÇ';
+  var semAcento = 'aaaaaeeeeiiiiooooouuuucAAAAAEEEEIIIIOOOOOUUUUC';
+
+  for (int i = 0; i < comAcento.length; i++) {
+    texto = texto.replaceAll(comAcento[i], semAcento[i]);
+  }
+  return texto;
+}
+
+String? funcaoCurso(String? curso) {
+
+  if(curso == null || curso == "") {
+    print("Curso inválido. Por favor, digite 'CC-Ciência da Computação' ou 'SI-Sistemas de Informação'.");
+    String? curso = stdin.readLineSync();
+    return funcaoCurso(curso);
+  }
+
+  String cursoNormalizado = removerAcentos(curso).toUpperCase();
+    if (cursoNormalizado != 'CC-CIENCIA DA COMPUTACAO' && cursoNormalizado != 'SI-SISTEMAS DE INFORMACAO') {
+      print("Curso inválido. Por favor, digite 'CC-Ciência da Computação' ou 'SI-Sistemas de Informação'.");
+      String? curso = stdin.readLineSync();
+      return funcaoCurso(curso);
+    }
+
+  return cursoNormalizado.toUpperCase();
+}
+
+
+while (true) {
+    print('-----------------------------------------------------------------------');
+    print('Digite o codigo do aluno com no máximo 4 digitos: (para sair digite 0000)');
+    String? codigo = stdin.readLineSync();
+    if (codigo == "0000") {
+      print('Programa encerrado.');
+      break;
+    }
+    String?  retornoCodigo = funcaoCodigo(codigo, codigos);
+
+    print("Digite o nome do aluno: ");
+    String? nome = stdin.readLineSync();
+    String? retornoNome = funcaoNome(nome);
+
+    print("Digite o sexo do candidato (M/F): ");
+    String? sexo = stdin.readLineSync();
+    String? retornoSexo = funcaoSexo(sexo);
+
+    print("Digite a pontuação do aluno: ");
+    String? pontuacao = stdin.readLineSync();
+    String? retornoPontuacao = funcaoPontuacao(pontuacao);
+
+    print("Digite o curso do aluno (CC-Ciência da Computação ou SI-Sistemas de Informação): ");
+    String? curso = stdin.readLineSync();
+    String? retornoCurso = funcaoCurso(curso);
+
+
+    if(int.parse(retornoPontuacao!) > 2500 && retornoCurso == 'CC-CIENCIA DA COMPUTACAO'){
+      alunos2500.add({
+        'codigo': retornoCodigo,
+        'nome': retornoNome,
+        'pontuacao': retornoPontuacao
+      });
     } 
 
-     if(aluno['sexo'] == 'M' && aluno['pontuação'] < menor_pontuacao_m ){
-      menor_pontuacao_m = aluno['pontuação'];
-      aluno_menor_pontuacao_m = " é ${aluno['nome']}  com $menor_pontuacao_m";
+     if(retornoSexo == 'M' && int.parse(retornoPontuacao) < menor_pontuacao_m ){
+      menor_pontuacao_m = int.parse(retornoPontuacao);
+      aluno_menor_pontuacao_m = " é ${retornoNome}  com $menor_pontuacao_m";
     }
 
-    if(aluno['sexo'] == 'M' && aluno['pontuação'] > maior_pontuacao_m && aluno['curso'] == 'SI-Sistemas de Informação'){
-      maior_pontuacao_m = aluno['pontuação'];
-      print("A maior pontuação entre os alunos de SI do sexo masculino é de ${aluno['nome']}, com código ${aluno['codigo']} com pontuação de $maior_pontuacao_m");
+    if(retornoSexo == 'M' && int.parse(retornoPontuacao) > maior_pontuacao_m && retornoCurso == 'SI-Sistemas de Informação'){
+      maior_pontuacao_m = int.parse(retornoPontuacao);
+      print("A maior pontuação entre os alunos de SI do sexo masculino é de ${retornoNome}, com código ${retornoCodigo} com pontuação de $maior_pontuacao_m");
     }
 
-    if(aluno['sexo'] == 'F'){
-      alunas_m++;
+    if(retornoSexo == 'F'){
+      alunas_f++;
     }else{
       alunos_m++;
     }
   }
 
-  print("Alunos que fizeram mais de 2500 pontos. $alunos_com_mais_de_2500_pontos");
+  print("Alunos com mais de 2500 pontos:");
+  for (var aluno in alunos2500) {
+    print("Código: ${aluno['codigo']}, Nome: ${aluno['nome']}, Pontuação: ${aluno['pontuacao']}");
+  }
   print("Aluno com menor pontuação entre os do sexo masculino: $aluno_menor_pontuacao_m");
-  double percentual_alunas = (alunas_m / (alunas_m + alunos_m)) * 100;
+  double percentual_alunas = (alunas_f / (alunas_f + alunos_m)) * 100;
   print("O percentual de alunas é de $percentual_alunas%");
-  double percentual_alunos = (alunos_m / (alunas_m + alunos_m)) * 100;
+  double percentual_alunos = (alunos_m / (alunas_f + alunos_m)) * 100;
   print("O percentual de alunos é de $percentual_alunos%");
 }
+
+
+
+
